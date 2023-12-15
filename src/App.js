bb303174e3f3c20c4756060ae820149cb7d49463
Prabesh Parajuli <prabesh1124@gmail.com>
2023-12-15 15:01:33 +0545

    Fetch_from_firebase

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react';
+
+import Tasks from './components/Tasks/Tasks';
+import NewTask from './components/NewTask/NewTask';
+import useHttp from './hooks/use_http';
+
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+
+// these three are what we are returning.
+  // creating alias.
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
+
+  useEffect(() => {
+    const transformTasks = (tasksObj) => {
+      const loadedTasks = [];
+
+      for (const taskKey in tasksObj) {
+        loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+      }
+
+      setTasks(loadedTasks);
+    };
+
+    fetchTasks(
+      { url: 'https://fir-http-project-6a213-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json' },
+      transformTasks
+    );
+  }, [fetchTasks]);
+
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task));
+  };
+
+  return (
+    <React.Fragment>
+      <NewTask onAddTask={taskAddHandler} />
+      <Tasks
+        items={tasks}
+        loading={isLoading}
+        error={error}
+        onFetch={fetchTasks}
+      />
+    </React.Fragment>
+  );
+}
+
+export default App;
